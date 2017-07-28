eb68db03befa4a89d644db130b220596741f6e5f
sharkeron <sharkeron@gmail.com>
2017-07-28 14:07:44 +0300

    Get data from server.
    p.s. until 10.05.17 API is private. Get request not work

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
+
+const omdb = require('../lib/omdb');
 
 function searchPath(req, res) {
+	const parsedUrl = url.parse(req.url, true);
+	const title = parsedUrl.query.title;
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
+
+	omdb.get(title, (error, movie) => {
+		if (error) throw error;
+
+		console.log(movie);
+	});
 
 	const stream = fs.createReadStream(path.resolve('public', 'movie.html'));
 
